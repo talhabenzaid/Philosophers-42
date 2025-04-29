9e2647aa83508c9d9b05a8ea0ce8ed38c46dd96e
talhabenzaid <talhabenzaid7@gmail.com>
2025-04-29 11:46:13 +0100

    all good

--- routine.c
+++ routine.c
@@ -6,7 +6,7 @@
 /*   By: tbenzaid <tbenzaid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 22:19:20 by tbenzaid          #+#    #+#             */
-/*   Updated: 2025/04/29 10:10:01 by tbenzaid         ###   ########.fr       */
+/*   Updated: 2025/04/29 11:44:11 by tbenzaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,7 @@ int check_death(t_philo *philo)
     nump = philo->info->number_philo;
     while(1)
     {
-        ft_usleep(1000,philo);
+        ft_usleep(500,philo);
         i = 0;
         while (i < nump)
         {
@@ -128,7 +128,7 @@ void *philosopher_routine(void *arg)
     t_philo *philo = (t_philo *)arg;
     philo->last_meal_time = get_current_time();
     if(philo->id % 2 == 0)
-        ft_usleep(500,philo);
+        ft_usleep(300,philo);
     while (1)
     {
         pthread_mutex_lock(&philo->info->dead_lock);
@@ -138,14 +138,12 @@ void *philosopher_routine(void *arg)
             break;
         }
         pthread_mutex_unlock(&philo->info->dead_lock);
-
         pthread_mutex_lock(philo->left_fork);
         if (!print_state(philo, "has taken a fork"))
         {
             pthread_mutex_unlock(philo->left_fork);
             return (NULL);
         }
-
         if (philo->info->number_philo == 1)
         {
             pthread_mutex_unlock(philo->left_fork);
@@ -153,7 +151,6 @@ void *philosopher_routine(void *arg)
             print_state(philo, "died");
             return (NULL);
         }
-
         pthread_mutex_lock(philo->right_fork);
         if (!print_state(philo, "has taken a fork"))
         {
@@ -161,7 +158,6 @@ void *philosopher_routine(void *arg)
             pthread_mutex_unlock(philo->right_fork);
             return (NULL);
         }
-
         if (!print_state(philo, "is eating"))
         {
             pthread_mutex_unlock(philo->left_fork);
@@ -171,15 +167,10 @@ void *philosopher_routine(void *arg)
         pthread_mutex_lock(&philo->info->dead_lock);
         philo->meals_eaten++;
         pthread_mutex_unlock(&philo->info->dead_lock);
-        
         philo->last_meal_time = get_current_time();
         ft_usleep(philo->info->time_to_eat,philo);
-        
-
         pthread_mutex_unlock(philo->right_fork);
         pthread_mutex_unlock(philo->left_fork);
-        
-        
         if (!print_state(philo, "is sleeping"))
             return (NULL);
         ft_usleep(philo->info->time_to_sleep,philo);
@@ -187,6 +178,5 @@ void *philosopher_routine(void *arg)
         if (!print_state(philo, "is thinking"))
             return (NULL);
     }
-
     return (NULL);
 }
